ccf932f66b89a58850f9c7dc08b0a4edb72bfea4
yumifoo <yufu336@gmail.com>
2019-12-07 23:33:40 +0200

    any p value less than 1e-323 set to 1e-323

--- hudson_miami.R
+++ hudson_miami.R
@@ -11,7 +11,6 @@ female <- fread(sprintf("BOLT_female_s184583_WhiteBritish.%s.bgen.stats.gz",trai
 male <- fread(sprintf("BOLT_male_s159112_WhiteBritish.%s.bgen.stats.gz",trait), colClasses = c(CHR = "character"))
 
 
-
 # change 23 to X
 female[female$CHR == "23",]$CHR <- rep("X", nrow(subset(female, CHR == "23")))
 male[male$CHR == "23",]$CHR <- rep("X", nrow(subset(male, CHR == "23")))
@@ -20,12 +19,13 @@ gwas.f <- data.frame(SNP = female$SNP,
                      CHR = female$CHR,
                      POS = female$BP,
                      pvalue = as.numeric(female$P_BOLT_LMM_INF))
+gwas.f[gwas.f$pvalue<1e-323,]$pvalue <- 1e-323
 
 gwas.m <- data.frame(SNP = male$SNP,
                      CHR = male$CHR,
                      POS = male$BP,
                      pvalue = as.numeric(male$P_BOLT_LMM_INF))
-
+gwas.m[gwas.m$pvalue<1e-323,]$pvalue <- 1e-323
 
 p <- gmirror(top = gwas.f, bottom = gwas.m,
         tline = 5e-8, bline = 5e-8,
