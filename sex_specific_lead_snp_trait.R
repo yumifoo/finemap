fa53a35407b57a4361550d871d9376d8422c4375
yumifoo <yufu336@gmail.com>
2019-12-08 22:55:58 +0200

    check if the lead snp file exists

--- sex_specific_lead_snp_trait.R
+++ sex_specific_lead_snp_trait.R
@@ -16,22 +16,24 @@ res.c <- fread(sprintf("BOLT_sex_combined_s343695_WhiteBritish.%s.bgen.stats.gz"
 
 for(ii in seq_along(chromosomes)){
   chr <- chromosomes[ii]
-  lead.snp <- read.table(sprintf("/fs/projects/ukbb/yu/BOLT_basicQT_agesq/%s/sex_combined/chr%d/lead_snp_cond/lead_snps_cond_chr%d.txt", trait, chr, chr),
-                         header = FALSE, stringsAsFactors = FALSE)
-  lead.snp.female <- subset(res.f, SNP %in% lead.snp[,1])
-  write.table(lead.snp.female, sprintf("/fs/projects/ukbb/yu/BOLT_basicQT_agesq/%s/sex_combined/chr%d/lead_snp_cond/BOLT_female_res_lead_snps_cond_chr%d.txt",trait, chr, chr),
-              row.names = FALSE, col.names = TRUE, quote = FALSE)
-  female <- rbind(female, lead.snp.female)
-  
-  lead.snp.male <- subset(res.m, SNP %in% lead.snp[,1])
-  write.table(lead.snp.male, sprintf("/fs/projects/ukbb/yu/BOLT_basicQT_agesq/%s/sex_combined/chr%d/lead_snp_cond/BOLT_male_res_lead_snps_cond_chr%d.txt", trait,chr, chr),
-              row.names = FALSE, col.names = TRUE, quote = FALSE)
-  male <- rbind(male, lead.snp.male)
-  
-  lead.snp.combine <- subset(res.c, SNP %in% lead.snp[,1])
-  write.table(lead.snp.combine, sprintf("/fs/projects/ukbb/yu/BOLT_basicQT_agesq/%s/sex_combined/chr%d/lead_snp_cond/BOLT_combine_res_lead_snps_cond_chr%d.txt",trait,chr, chr),
-              row.names = FALSE, col.names = TRUE, quote = FALSE)
-  combine <- rbind(combine, lead.snp.combine)
+  if(file.exists(sprintf("/fs/projects/ukbb/yu/BOLT_basicQT_agesq/%s/sex_combined/chr%d/lead_snp_cond/lead_snps_cond_chr%d.txt", trait, chr, chr))){ # check if file exists
+    lead.snp <- read.table(sprintf("/fs/projects/ukbb/yu/BOLT_basicQT_agesq/%s/sex_combined/chr%d/lead_snp_cond/lead_snps_cond_chr%d.txt", trait, chr, chr),
+                           header = FALSE, stringsAsFactors = FALSE)
+    lead.snp.female <- subset(res.f, SNP %in% lead.snp[,1])
+    write.table(lead.snp.female, sprintf("/fs/projects/ukbb/yu/BOLT_basicQT_agesq/%s/sex_combined/chr%d/lead_snp_cond/BOLT_female_res_lead_snps_cond_chr%d.txt",trait, chr, chr),
+                row.names = FALSE, col.names = TRUE, quote = FALSE)
+    female <- rbind(female, lead.snp.female)
+    
+    lead.snp.male <- subset(res.m, SNP %in% lead.snp[,1])
+    write.table(lead.snp.male, sprintf("/fs/projects/ukbb/yu/BOLT_basicQT_agesq/%s/sex_combined/chr%d/lead_snp_cond/BOLT_male_res_lead_snps_cond_chr%d.txt", trait,chr, chr),
+                row.names = FALSE, col.names = TRUE, quote = FALSE)
+    male <- rbind(male, lead.snp.male)
+    
+    lead.snp.combine <- subset(res.c, SNP %in% lead.snp[,1])
+    write.table(lead.snp.combine, sprintf("/fs/projects/ukbb/yu/BOLT_basicQT_agesq/%s/sex_combined/chr%d/lead_snp_cond/BOLT_combine_res_lead_snps_cond_chr%d.txt",trait,chr, chr),
+                row.names = FALSE, col.names = TRUE, quote = FALSE)
+    combine <- rbind(combine, lead.snp.combine)
+  }
 }
 write.table(female,sprintf("/fs/projects/ukbb/yu/BOLT_basicQT_agesq/%s/sex_combined/BOLT_female_res_lead_snps_cond.txt", trait),
             row.names = FALSE, col.names = TRUE, quote = FALSE)
